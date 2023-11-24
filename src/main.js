2b191a75d666a0ee2f81c09d807a6a7a3934f77d
Andrii Veremii <dashuk10@gmail.com>
2023-11-24 16:18:38 +0100

    add

--- src/main.js
+++ src/main.js
@@ -9,9 +9,11 @@ const refs = {
   form: document.querySelector('#search-form'),
   input: document.querySelector('input'),
   gallery: document.querySelector('.gallery'),
+  loadBtn: document.querySelector('.js-load-btn'),
 };
 
 refs.form.addEventListener('submit', handleSearch);
+refs.loadBtn.addEventListener('click', handleLoadMore);
 
 const onImages = new SimpleLightbox('.js-gallery a', {
   captionDelay: 250,
@@ -24,6 +26,7 @@ let page = 1;
 async function handleSearch(e) {
   e.preventDefault();
   clearPage();
+  
   const query = refs.input.value.trim().toLowerCase();
 
   if (!query) {
@@ -35,10 +38,19 @@ async function handleSearch(e) {
     return;
   }
 
-  page = 1;
+   if ((currentQuery === query)) {
+     iziToast.show({
+       title: 'Ops!',
+       message: 'Enter something to search!',
+       position: 'topRight',
+     });
+     return;
+   }
+
+  currentQuery = query;
 
   try {
-    const data = await getImage(query, page);
+    const data = await getImage(currentQuery, page);
 
     const allCollection = data.data.totalHits;
     const collection = data.data.hits;
@@ -52,7 +64,7 @@ async function handleSearch(e) {
       return;
     }
 
-    refs.gallery.insertAdjacentHTML('beforeend', markupGallery(collection));
+    refs.gallery.insertAdjacentHTML('afterbegin', markupGallery(collection));
     iziToast.show({
       title: 'Hey',
       message: `Hooray! We found ${allCollection} images.`,
@@ -65,8 +77,18 @@ async function handleSearch(e) {
   }
 }
 
+async function handleLoadMore(e) {
+  e.preventDefault();
+  page += 1;
+  const data = await getImage(currentQuery, page);
+  const collection = data.data.hits;
+  refs.gallery.insertAdjacentHTML('beforeend', markupGallery(collection));
+}
+
 function clearPage() {
   refs.gallery.innerHTML = '';
+  currentQuery = '';
+  page = 1;
 }
 
 const scrollPage = () => {
